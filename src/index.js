33b3cf09bd6439fc7306837ed816dd9be9e20bab
wawandx <rachmad.kurniawan@ralali.com>
2019-07-12 16:01:47 +0700

    css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import Calculator from '../src/components/calculator';
 import * as serviceWorker from './serviceWorker';
 
